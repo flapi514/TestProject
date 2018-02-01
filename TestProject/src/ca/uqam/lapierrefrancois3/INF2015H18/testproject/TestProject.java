e8f550203fe41739d2aa5f80575a2240ce67a88d
Francois Lapierre <lapierre.francois.3@courrier.uqam.ca>
2018-02-01 18:42:06 -0500

    TestProject: Messing up structure

---
+++ TestProject/src/ca/uqam/lapierrefrancois3/INF2015H18/testproject/TestProject.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.uqam.lapierrefrancois3.INF2015H18.testproject;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author flapi
+ */
+public class TestProject {
+    
+    private final Connection con;
+    
+    TestProject(){
+       
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TestProject project = new TestProject();
+    }
+    
+}
